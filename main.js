f165242eb4dd5dc9fa5038a40e759eab48eac39d
SELENDRA KUMAR <76847651+wolf3766@users.noreply.github.com>
2022-11-07 21:14:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -130,7 +130,7 @@ let leaveChannel=async () =>{
 }
 
 let toggleCamera=async()=>{
-    let videoTrack=localStream.getTracks().find(track=>track.kind=='video') 
+    let videoTrack=localStream.getTracks().find((track)=>track.kind=='video') 
     if(videoTrack.enabled){
         videoTrack.enabled=false
         document.getElementById("camera-btn").style.backgroundColor="rgb(255,80,80)"
@@ -141,7 +141,7 @@ let toggleCamera=async()=>{
 }
 
 let toggleMic=async()=>{
-    let audioTrack=localStream.getTracks().find(track=>track.kind=='audio') 
+    let audioTrack=localStream.getTracks().find((track)=>track.kind=='audio') 
     if(audioTrack.enabled){
         audioTrack.enabled=false
         document.getElementById("mic-btn").style.backgroundColor="rgb(255,80,80)"
